dd9fb9903ff4d7828c8c870029650c68f1d2aad3
MofFoam <2397521545@qq.com>
2023-07-23 20:45:06 +0800

    param

--- user-service/src/main/java/com/fmwyc/user/controller/TestController.java
+++ user-service/src/main/java/com/fmwyc/user/controller/TestController.java
@@ -1,13 +1,14 @@
 package com.fmwyc.user.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
     @RequestMapping("/test")
-    public String test(){
+    public String test(@RequestParam String token){
         return "Hello World";
     }
 
